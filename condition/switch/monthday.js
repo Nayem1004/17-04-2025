87bc4f39505f491ffd5373cb58125ac683edb211
Nayem1004 <nayem@procoder.org>
2025-04-17 16:23:20 +0600

    quater add

--- condition/switch/monthday.js
+++ condition/switch/monthday.js
@@ -5,40 +5,41 @@ var month = today.getMonth()
 switch(month)
 {
     case 1: 
-        month = (" january 31 days");
+        month = (" january 31 days" + " " + "and this month is 1st  quater");
+
         break;
     case 2: 
-        month = (" February 28/29 days");
+        month = (" February 28/29 days" + " " + "and this month is 1st  quater");
         break;
     case 3: 
-        month = (" march 31 days");
+        month = (" march 31 days" + " " + "and this month is 1st  quater");
         break;
     case 4: 
-        month = ("April 30 days");
+        month = ("April 30 days" + " " + "and this month is 2nd  quater");
         break;
     case 5: 
-        month = ("May 31 days");
+        month = ("May 31 days" + " " + "and this month is 2nd  quater");
         break;
     case 6: 
-        month = ("Jun 30 days");
+        month = ("Jun 30 days" + " " + "and this month is 2nd  quater");
         break;
     case 7: 
-        month = ("Julay 31 days");
+        month = ("Julay 31 days" + " " + "and this month is 3rd  quater");
         break;
     case 8: 
-        month = ("Agusht 31 days");
+        month = ("Agusht 31 days" + " " + "and this month is 3rd  quater");
         break;
     case 9: 
-        month = ("Septembor 30 days");
+        month = ("Septembor 30 days" + " " + "and this month is 3rd  quater");
         break;
     case 10: 
-        month = ("Actobor 31 days");
+        month = ("Actobor 31 days" + " " + "and this month is 4th  quater");
         break;
     case 11: 
-        month = ("Novembor 30 days");
+        month = ("Novembor 30 days" + " " + "and this month is 4th  quater");
         break;
     case 12: 
-        month = ("Desembor 31 days");
+        month = ("Desembor 31 days" + " " + "and this month is 4th  quater");
         break;
     default: 
         month = ("Invalid input! Please enter month number between 1-12");
