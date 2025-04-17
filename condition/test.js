746dc0fa6ae51c4e096ceacf7a03a2ec1443c38e
Nayem1004 <nayem@procoder.org>
2025-04-17 12:11:19 +0600

    new file added test.js

---
+++ condition/test.js
@@ -0,0 +1,11 @@
+let num = 0
+if (num > 0) {
+    console.log('output = "The number is Positive."');
+  } else if (num < 0) {
+   console.log ('output = "The number is Negative."');
+  } else if (num === 0) {
+    console.log('output = "The number is Zero."');
+  } else {
+    console.log('output = "Please enter a valid number."');
+  }
+  
