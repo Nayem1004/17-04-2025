20596308218e1c9d08a8474623c5c265eba6fe0f
Nayem1004 <nayem@procoder.org>
2025-04-17 16:35:12 +0600

    change this code for new logic month quater show

--- condition/switch/monthday.js
+++ condition/switch/monthday.js
@@ -5,41 +5,14 @@ var month = today.getMonth()
 switch(month)
 {
     case 1: 
-        month = (" january 31 days" + " " + "and this month is 1st  quater");
-
-        break;
     case 2: 
-        month = (" February 28/29 days" + " " + "and this month is 1st  quater");
-        break;
     case 3: 
-        month = (" march 31 days" + " " + "and this month is 1st  quater");
+        month = ("this month is 1st  quater");
         break;
     case 4: 
-        month = ("April 30 days" + " " + "and this month is 2nd  quater");
-        break;
-    case 5: 
-        month = ("May 31 days" + " " + "and this month is 2nd  quater");
-        break;
+    case 5:
     case 6: 
-        month = ("Jun 30 days" + " " + "and this month is 2nd  quater");
-        break;
-    case 7: 
-        month = ("Julay 31 days" + " " + "and this month is 3rd  quater");
-        break;
-    case 8: 
-        month = ("Agusht 31 days" + " " + "and this month is 3rd  quater");
-        break;
-    case 9: 
-        month = ("Septembor 30 days" + " " + "and this month is 3rd  quater");
-        break;
-    case 10: 
-        month = ("Actobor 31 days" + " " + "and this month is 4th  quater");
-        break;
-    case 11: 
-        month = ("Novembor 30 days" + " " + "and this month is 4th  quater");
-        break;
-    case 12: 
-        month = ("Desembor 31 days" + " " + "and this month is 4th  quater");
+        month = ("this month is 2nd quater");
         break;
     default: 
         month = ("Invalid input! Please enter month number between 1-12");
